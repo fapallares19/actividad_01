851feda4fc2d3d624cc09aa95f42070e49b5ba2c
fapallares19 <142366964+fapallares19@users.noreply.github.com>
2025-02-12 10:01:34 -0500

    Update flutter_course_page.dart

--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -28,10 +28,54 @@ class FlutterCoursePage extends StatelessWidget {
   }
 
   Widget buildModules() {
-    return const Text("Modules");
+   
+    final modules = [
+      {'icon': Icons.book, 'label': 'Introduction'},
+      {'icon': Icons.design_services, 'label': 'UX design'},
+      {'icon': Icons.storage, 'label': 'State management'},
+      {'icon': Icons.bug_report, 'label': 'Testing'},
+      {'icon': Icons.network_check, 'label': 'Networking'},
+    ];
+
+    return SizedBox(
+      height: 100,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: modules.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Colors.grey[200],
+                  child: Icon(modules[index]['icon'] as IconData, size: 30, color: Colors.black),
+                ),
+                const SizedBox(height: 4),
+                Text(modules[index]['label'] as String),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 
   Widget buildProjectList() {
-    return const Text("Projects");
+   final projects = ['Sudoku', 'Random user', 'Note tak', 'Weather', 'Delivery app'];
+    
+    return ListView.builder(
+      itemCount: projects.length,
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            leading: const Icon(Icons.folder, color: Colors.blue),
+            title: Text(projects[index]),
+            trailing: const Icon(Icons.more_vert),
+          ),
+        );
+      },
+    );
   }
 }
